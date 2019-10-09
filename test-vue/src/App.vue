706f39593bfe78a01ef4e62a3aced6ea8e9b07a6
Tim Pinkerton <timpinkerton@mail.com>
2019-10-08 23:40:46 -0400

    testing addition of second component

--- test-vue/src/App.vue
+++ test-vue/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <p>{{ msg }}</p>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Second/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Second from './components/Second.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Second
+  },
+  data() {
+    return {
+      msg: 'what??!?!'
+    }
   }
 }
 </script>
